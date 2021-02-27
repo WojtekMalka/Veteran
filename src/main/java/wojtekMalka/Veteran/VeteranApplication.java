31c7c9be5f9f75aa1b01d13c32005ddecb76900a
WojtekMalka <malka_wojciech@wp.pl>
2021-02-27 18:49:53 +0100

    initial commit

---
+++ src/main/java/wojtekMalka/Veteran/VeteranApplication.java
@@ -0,0 +1,13 @@
+package wojtekMalka.Veteran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeteranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeteranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wojtekMalka/Veteran/VeteranApplicationTests.java
@@ -0,0 +1,13 @@
+package wojtekMalka.Veteran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeteranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
