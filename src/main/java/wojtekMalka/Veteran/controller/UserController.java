fc58d62b2103bd33d71c493c55ee323833fb92dd
WojtekMalka <malka_wojciech@wp.pl>
2021-02-27 19:44:46 +0100

    controller package added

---
+++ src/main/java/wojtekMalka/Veteran/controller/UserController.java
@@ -0,0 +1,7 @@
+package wojtekMalka.Veteran.controller;
+
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+@RepositoryRestController
+public class UserController {
+}
--- src/main/java/wojtekMalka/Veteran/repository/UserRepository.java
+++ src/main/java/wojtekMalka/Veteran/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package wojtekMalka.Veteran.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import wojtekMalka.Veteran.entities.User;
 
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long user_id);
+
+/*    @RestResource(path = "user")
+    User findUserByUser_id(@Param("userId") Long user_id);*/
 }
