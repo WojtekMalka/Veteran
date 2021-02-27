fe6a3c1898bd92f93da4792789e3fee4885eccaa
WojtekMalka <malka_wojciech@wp.pl>
2021-02-27 19:11:59 +0100

    POM modified and repository created

--- src/main/java/wojtekMalka/Veteran/entities/User.java
+++ src/main/java/wojtekMalka/Veteran/entities/User.java
@@ -1,4 +1,42 @@
 package wojtekMalka.Veteran.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long user_id;
+    private String login;
+    private Integer basket_id;
+
+    public User() {
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Integer getBasket_id() {
+        return basket_id;
+    }
+
+    public void setBasket_id(Integer basket_id) {
+        this.basket_id = basket_id;
+    }
 }
