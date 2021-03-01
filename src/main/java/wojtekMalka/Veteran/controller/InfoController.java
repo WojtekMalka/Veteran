fd7c2f872fe1f45a5ec7927ff7dae0e511d41e85
WojtekMalka <malka_wojciech@wp.pl>
2021-03-01 15:47:16 +0100

    info controller Autowire anotation added

--- src/main/java/wojtekMalka/Veteran/controller/InfoController.java
+++ src/main/java/wojtekMalka/Veteran/controller/InfoController.java
@@ -1,19 +1,21 @@
 package wojtekMalka.Veteran.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
-    @Value("${spring.datasource.url}")
-    public String url;
+    @Autowired
+    public DataSourceProperties dataSourceProperties;
     @Value("${my.prop}")
     public String myProp;
 
     @GetMapping("/info/url")
     String url() {
-        return url;
+        return dataSourceProperties.getUrl();
     }
 
     @GetMapping("/info/prop")
